d78c94285af2cd8e0ba5266432ad5d893566a2b9
Radu Almășan <radu.almasan@ness.com>
2016-08-30 17:21:10 +0300

    lesson3 - 1. TapeEquilibrium

---
+++ src/main/java/com/radualmasan/codility/lesson2/TapeEquilibrium.java
@@ -0,0 +1,29 @@
+package com.radualmasan.codility.lesson2;
+
+import java.util.Arrays;
+
+public class TapeEquilibrium {
+
+    public int findLowestSum(final int[] ints) {
+        int leftSum = ints[0];
+        int rightSum = Arrays.stream(ints, 1, ints.length).sum();
+        int minDiff = Math.abs(leftSum - rightSum);
+
+        if (minDiff == 0)
+            return 0;
+
+        for (int i = 2; i < ints.length; i++) {
+            leftSum += ints[i - 1];
+            rightSum -= ints[i - 1];
+            int currentDiff = Math.abs(leftSum - rightSum);
+
+            if (currentDiff == 0)
+                return 0;
+
+            if (minDiff > currentDiff)
+                minDiff = currentDiff;
+        }
+
+        return minDiff;
+    }
+}
---
+++ src/test/java/com/radualmasan/codility/lesson2/TapeEquilibriumTest.java
@@ -0,0 +1,35 @@
+package com.radualmasan.codility.lesson2;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TapeEquilibriumTest {
+
+    private TapeEquilibrium tapeEquilibrium = new TapeEquilibrium();
+
+    @Test
+    public void testArrayWithTwoElements() throws Exception {
+        assertEquals(1, tapeEquilibrium.findLowestSum(new int[]{1, 2}));
+    }
+
+    @Test
+    public void testGivenExample() throws Exception {
+        assertEquals(1, tapeEquilibrium.findLowestSum(new int[]{3, 1, 2, 4, 3}));
+    }
+
+    @Test
+    public void testRightmostCase() throws Exception {
+        assertEquals(0, tapeEquilibrium.findLowestSum(new int[]{10, 5, 5, 20, 40}));
+    }
+
+    @Test
+    public void testLeftmostCase() throws Exception {
+        assertEquals(1, tapeEquilibrium.findLowestSum(new int[]{41, 10, 5, 5, 20}));
+    }
+
+    @Test
+    public void testLeftmostIsPerfectBalance() throws Exception {
+        assertEquals(0, tapeEquilibrium.findLowestSum(new int[]{40, 10, 5, 5, 20}));
+    }
+}
